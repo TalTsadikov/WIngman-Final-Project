5fc7e4c959a4f20cdcb1780546df946d37a4680e
daniel9771 <97406982+daniel9771@users.noreply.github.com>
2022-07-02 14:27:05 +0300

    added coins and score

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Destroy(this.gameObject);
             Debug.Log("Collected");
+            TotalScore.levelScore++;
         }
     }
 }
