65969ba5fe5a604500fd1f4ee4fb24f302a76b95
TalTsadikov <taltsadikov@gmail.com>
2022-07-10 22:09:04 +0300

    small changes fix camera shadow

--- Assets/Scripts/Objects Scripts/Coin.cs
+++ Assets/Scripts/Objects Scripts/Coin.cs
@@ -7,23 +7,6 @@ public class Coin : MonoBehaviour//, IDataPersistence
     [SerializeField] CoinManager coinManager;
     private bool collected = false;
 
-   //[SerializeField] private string id;
-   //[ContextMenu("Generate guid for id")]
-   //private void GenerateGuid()
-   //{
-   //    id = System.Guid.NewGuid().ToString();
-   //}
-
-  // public void LoadData(GameData data)
-  // {
-  //     coinManager.coinsCollected = data.coinsCollected;
-  // }
-  //
-  // public void SaveData(GameData data)
-  // {
-  //     data.coinsCollected = coinManager.coinsCollected;
-  // }
-
     private void OnTriggerEnter(Collider other)
     {
         if (!collected && other.tag == "Player")
