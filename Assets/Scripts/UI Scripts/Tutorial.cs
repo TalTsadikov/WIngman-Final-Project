58ce6d8641b5307823df1d31c2b0b635ea0bb81a
TalTsadikov <taltsadikov@gmail.com>
2022-07-11 17:57:58 +0300

    Update Tutorial.cs

--- Assets/Scripts/UI Scripts/Tutorial.cs
+++ Assets/Scripts/UI Scripts/Tutorial.cs
@@ -15,12 +15,10 @@ public class Tutorial : MonoBehaviour
             tutorialScreen.SetActive(true);
             Time.timeScale = 0f;
             PlayerPrefs.SetInt("firstTime", 1);
-            Debug.Log("First");
         }
         else
         {
             tutorialScreen.SetActive(false);
-            Debug.Log("Not First");
         }
     }
 
